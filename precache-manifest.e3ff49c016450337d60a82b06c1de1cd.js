3d002f1fb40489c3936bbf98e233378371485a33
DumanTorbayev <dumanchikt@gmail.com>
2020-08-31 15:59:09 +0600

    Updates

---
+++ precache-manifest.e3ff49c016450337d60a82b06c1de1cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35ff80c7ebd3314477eccdb1c6329a3",
+    "url": "/funbox-test-app/index.html"
+  },
+  {
+    "revision": "5fee8d20fd8019cb4bd5",
+    "url": "/funbox-test-app/static/css/main.f55b5bd8.chunk.css"
+  },
+  {
+    "revision": "1e0039a524ca57ca7022",
+    "url": "/funbox-test-app/static/js/2.a9da2c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funbox-test-app/static/js/2.a9da2c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fee8d20fd8019cb4bd5",
+    "url": "/funbox-test-app/static/js/main.d79612c0.chunk.js"
+  },
+  {
+    "revision": "7402a4285d4f70d951bb",
+    "url": "/funbox-test-app/static/js/runtime-main.80ae894c.js"
+  },
+  {
+    "revision": "32f36dc58061845cef2866c7171838f3",
+    "url": "/funbox-test-app/static/media/Exo2Thin.32f36dc5.ttf"
+  },
+  {
+    "revision": "5a50f7c24ee67fcb264f5d1cbb4b4892",
+    "url": "/funbox-test-app/static/media/Exo2Thin.5a50f7c2.woff"
+  },
+  {
+    "revision": "8c7f03bb9178614bed22a5f42e118507",
+    "url": "/funbox-test-app/static/media/Exo2Thin.8c7f03bb.eot"
+  },
+  {
+    "revision": "128b18468ecb64ed43af0c593b6b4f77",
+    "url": "/funbox-test-app/static/media/TrebuchetMS.128b1846.woff"
+  },
+  {
+    "revision": "48c4ef39806ef76eaeced814b8a5705c",
+    "url": "/funbox-test-app/static/media/TrebuchetMS.48c4ef39.ttf"
+  },
+  {
+    "revision": "6f5db1a6d4539423956478f0dd4ada01",
+    "url": "/funbox-test-app/static/media/background.6f5db1a6.png"
+  },
+  {
+    "revision": "43c8985a06542982c708a0e559560700",
+    "url": "/funbox-test-app/static/media/cat.43c8985a.png"
+  }
+]);
